bec58a2f4b5ffd9ba20b755484a6da0dc3b2ea9c
崔启坤 <todogo@vip.163.com>
2016-08-07 23:22:10 +0800

    add some snippets

--- base_app.js
+++ base_app.js
@@ -1,58 +1,20 @@
-var express = require('express');
-var path = require('path');
-var _ = require('lodash');
-
-var bodyParser = require('body-parser');
-var helmet = require('helmet');
-
-var config = require('./config');
 var logger = require('./common/logger');
-var app_router = require('./app_router');
 
-var app = express();
 
-// 本系统的资源
-var publicDir = path.join(__dirname, 'public');
-// 第三方的资源
-var staticDir = path.join(__dirname, 'bower_components');
-// 打包后的文件目录
-var distDir = path.join(__dirname, 'dist');
+/**
+ * 防止出现意外没有想到的错误使整个程序崩溃
+ * @param  {[type]} 'uncaughtException' [description]
+ * @param  {[type]} function(err        [description]
+ * @return {[type]}                     [description]
+ */
+process.on('uncaughtException',function(err){
 
-// express.static 是 express 唯一一个内置的中间件
-// public 是挂载路径,是对外界生效的, 不是指本地public的意思,本地的这个public 是在上面语句中指定的
-// 这个方法可以多次调用, 查找是按照添加的顺序查找
-app.use('/public', express.static(staticDir));
-// 有些资源需要打包后使用
-app.use('/dist', express.static(distDir));
-
-app.use(bodyParser.json({
-    limit: '1mb'
-}));
-app.use(bodyParser.urlencoded({
-    extended: true,
-    limit: '1mb'
-}));
-
-// 视图目录
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
-app.engine('html', require('ejs').__express);
-
-// 网站安全增强
-app.use(helmet());
-
-_.extend(app.locals, {
-    config: config
 });
 
-// router
-app.use('/', app_router);
-
-var server = app.listen(config.port, function() {
-    logger.info('listening on port', config.port);
-    logger.info('You can debug your app with http://' + config.hostname + ':' + config.port);
-    logger.info('');
-});
-
-exports.app = app;
-exports.server = server;
+/**
+ * 该对象持有app中一些配置的引用
+ * @type {Object}
+ */
+global.xx={
+  logger:logger,
+};
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 var base_app = require('./base_app');
+var base_web = require('./base_web');
