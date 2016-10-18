5ad2d9594f9ff261641307043210794c139b60b0
崔启坤 <todogo@vip.163.com>
2016-10-18 22:42:01 +0800

    上次提交忘提交的文件

--- front.src/public/js/main.js
+++ front.src/public/js/main.js
@@ -1,24 +1,47 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 import SettingMenuView from './components/setting-menu-view.vue'
 import ServerStatusview from './components/server-status-view.vue'
+import ManageRoleView from './components/manage-role-view.vue'
+import RoleTableView from './components/manage/role/role-table-view.vue'
+import RoleCreateView from './components/manage/role/role-create-view.vue'
 
+import Element from 'element-ui'
+import 'element-ui/lib/theme-default/index.css';
+Vue.use(Element);
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 Vue.config.devtools = true;
 
 var router = new VueRouter({
     routes: [
         {
-            path: '/setting/menu',
+            path: '/manage/menu',
             component: SettingMenuView
         }, {
-            path: '/setting/permission',
+            path: '/manage/permission',
             component: SettingMenuView
         }, {
             path: '/monitor/server',
             component: ServerStatusview
+        }, {
+            path: '/manage/role',
+            component: ManageRoleView,
+            children: [
+                {
+                    path: '', // 空路径 是会默认呈现的路径
+                    component: RoleTableView
+                }, {
+                    path: 'roleTable',
+                    component: RoleTableView
+                }, {
+                    path: 'roleCreate',
+                    component: RoleCreateView
+                }
+            ]
         }
     ]
 });
