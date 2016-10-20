f1e909061996ac6357d094955c2f775c5a6b0109
崔启坤 <todogo@vip.163.com>
2016-10-20 20:57:54 +0800

    add 用户管理页面，add 面包削导航,add vuex (面包削导航使用了vuex 管理属性)

---
+++ front.src/public/js/components/manage/user/user-create.vue
@@ -0,0 +1,58 @@
+<template lang="html">
+    <div class="row">
+        <div class="col-sm-5 col-sm-offset-2">
+            <el-form ref="form" label-width="80px" @submit.prevent="onSubmit">
+              <el-form-item label="用户名">
+                <el-input v-model='name'></el-input>
+              </el-form-item>
+              <el-form-item label="登陆名">
+                <el-input v-model='loginname'></el-input>
+              </el-form-item>
+              <el-form-item label="初始密码">
+                <el-input v-model='password'></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button @click.native='saveUser' type="primary">立即创建</el-button>
+                <el-button @click.native.back='back'>取消</el-button>
+              </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+
+        return {
+            name: null,
+            loginname: null,
+            password: null
+        }
+
+    },
+    computed: {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+        saveUser: function() {
+
+            console.log(this.$data);
+
+        },
+        back: function() {
+            this.$router.push('/manage/user/userTable')
+        }
+    },
+    components: {
+
+    }
+}
+</script>
+
+<style lang="css">
+
+</style>
--- front.src/public/js/main.js
+++ front.src/public/js/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import App from './App.vue'
+
+import store from './store';
+
 import SettingMenuView from './components/setting-menu-view.vue'
 import ServerStatusview from './components/server-status-view.vue'
 import ManageRoleView from './components/manage-role-view.vue'
@@ -9,7 +12,10 @@ import RoleTableView from './components/manage/role/role-table-view.vue'
 import RoleCreateView from './components/manage/role/role-create-view.vue'
 import PathView from './components/manage/path/path-view.vue';
 import AuthorityView from './components/manage/authority/authority-view.vue'
+
 import ManageUserView from './components/manage/user/user-view.vue'
+import UserTable from './components/manage/user/user-table.vue'
+import UserCreate from './components/manage/user/user-create.vue'
 
 import Element from 'element-ui'
 import 'element-ui/lib/theme-default/index.css';
@@ -23,7 +29,37 @@ var router = new VueRouter({
     routes: [
         {
             path: '/manage/user',
-            component: ManageUserView
+            component: ManageUserView,
+            children: [
+                {
+                    path: '',
+                    component: UserTable,
+                    meta: {
+                        breadcrumbName: [
+                            '主页', '系统管理', '用户管理'
+                        ],
+                        breadcrumbUrl: ['/', '', '/manage/user']
+                    }
+                }, {
+                    path: 'userTable',
+                    component: UserTable,
+                    meta: {
+                        breadcrumbName: [
+                            '主页', '系统管理', '用户管理'
+                        ],
+                        breadcrumbUrl: ['/', '', '/manage/user']
+                    }
+                }, {
+                    path: 'userCreate',
+                    component: UserCreate,
+                    meta: {
+                        breadcrumbName: [
+                            '主页', '系统管理', '用户管理', '用户创建'
+                        ],
+                        breadcrumbUrl: ['/', '', '/manage/user', '/manage/user/userCreate']
+                    }
+                }
+            ]
         }, {
             path: '/manage/menu',
             component: SettingMenuView
@@ -35,7 +71,13 @@ var router = new VueRouter({
             component: ServerStatusview
         }, {
             path: '/manage/path',
-            component: PathView
+            component: PathView,
+            meta: {
+                breadcrumbName: [
+                    '主页', '系统管理', '路径管理'
+                ],
+                breadcrumbUrl: ['/', '', '/manage/path']
+            }
         }, {
             path: '/manage/authority',
             component: AuthorityView
@@ -45,21 +87,47 @@ var router = new VueRouter({
             children: [
                 {
                     path: '', // 空路径 是会默认呈现的路径
-                    component: RoleTableView
+                    component: RoleTableView,
+                    meta: {
+                        breadcrumbName: [
+                            '主页', '系统管理', '角色管理'
+                        ],
+                        breadcrumbUrl: ['/', '', '/manage/role']
+                    }
                 }, {
                     path: 'roleTable',
-                    component: RoleTableView
+                    component: RoleTableView,
+                    meta: {
+                        breadcrumbName: [
+                            '主页', '系统管理', '用户管理'
+                        ],
+                        breadcrumbUrl: ['/', '', '/manage/role']
+                    }
                 }, {
                     path: 'roleCreate',
-                    component: RoleCreateView
+                    component: RoleCreateView,
+                    meta: {
+                        breadcrumbName: [
+                            '主页', '系统管理', '角色管理','角色创建'
+                        ],
+                        breadcrumbUrl: ['/', '', '/manage/role','/manage/role/roleCreate']
+                    }
                 }
             ]
         }
     ]
 });
 
+// 路由切换的时候，需要更新breadcrumb的信息
+router.beforeEach((to, from, next) => {
+
+    router.app.$store.commit('BREADCRUMBPATHREFRESH', to);
+    next();
+})
+
 new Vue({
     el: '#app',
+    store,
     router: router,
     render: h => h(App)
 })
