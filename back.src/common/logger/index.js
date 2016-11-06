3b53ddacfd49a474473556b259ad083adc7739d2
xunull <todogo@vip.163.com>
2016-11-06 09:40:53 +0800

    winston 添加filter 增加颜色的输出，因为对winston本身的level 设置颜色 不起作用

--- back.src/common/logger/index.js
+++ back.src/common/logger/index.js
@@ -1,5 +1,9 @@
+var colors = require('colors/safe');
+
 var winstonLogger = require('./winston');
 
 var log4jsLogger = require('./log4js');
 
-module.exports = log4jsLogger;
+const logger = winstonLogger;
+
+module.exports = winstonLogger;
--- back.src/common/logger/winston.js
+++ back.src/common/logger/winston.js
@@ -1,14 +1,49 @@
 var winston = require('winston');
+var colors = require('colors/safe');
 var config = require('../../../config/app.config');
+
+// var colors = {
+//     error: 'red',
+//     warn: 'red',
+//     info: 'blue',
+//     verbose: 'green',
+//     debug: 'green',
+//     silly: 'white'
+// }
 
 var logger = new(winston.Logger)({
     level: config.logger.level,
+    // colors:colors, 对于内置的这些logger 颜色设置不起作用
     transports: [
-        new(winston.transports.Console)({
-        }),
+        new(winston.transports.Console)({}),
         new(winston.transports.File)({name: 'log-file', filename: config.logger.logFile}),
         new(winston.transports.File)({name: 'error-file', filename: config.logger.errorFile, level: 'error'})
     ]
+});
+
+/**
+ * logger 的颜色设置
+ */
+logger.filters.push(function(level, msg, meta) {
+    switch (level) {
+        case 'debug':
+            return colors.blue(msg);
+            break;
+        case 'verbose':
+            return colors.blue(msg);
+            break;
+        case 'info':
+            return colors.green(msg);
+            break;
+        case 'warn':
+            return colors.yellow(msg);
+            break;
+        case 'error':
+            return colors.red(msg);
+            break;
+        default:
+            return msg;
+    }
 });
 
 module.exports = logger;
