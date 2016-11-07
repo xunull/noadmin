c24db1347a13f1fe3e127c7e002fb42af933668b
xunull <todogo@vip.163.com>
2016-11-07 15:24:33 +0800

    winston logger 添加时间戳

--- back.src/business/yuwang/index.js
+++ back.src/business/yuwang/index.js
@@ -22,17 +22,3 @@ class YuwangBusiness extends Business {
 }
 
 module.exports = YuwangBusiness;
-
-// targetSite.start();
-
-/**
- * 对于business的index文件
- * 如果仅仅在业务加载的地方 require('index')
- * 那么业务的实际加载过程完全是由各个业务自己实现
- * 如果想要干预业务中的logger 比如说替换logger，为其赋予一个新的logger(带有相关业务category的logger)
- * 此替换操作可能并不会很完美，因为业务逻辑已经被加载过了
- *
- * 或者说require('index')时，使其不加载自身的业务逻辑
- * 比如说require('index')返回的是一个类，需要new 才可以使用
- * 那么就可以在new 创建的时候 侵入自己的某些预实现
- */
